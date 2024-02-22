2cf87c161300ad89182bbfb79f5a4260c46b3213
marceloacosta <acosta.cavalero@gmail.com>
2024-02-22 13:23:54 -0300

    changed to cloud solutions theme

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://blockchain.zircon.tech",
+  site: "https://cloud.zircon.tech",
   integrations: [tailwind(), mdx(), sitemap()],
 });
